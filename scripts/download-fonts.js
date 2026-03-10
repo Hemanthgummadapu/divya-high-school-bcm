a10a279f5c37e94e01f08edd11f6ca875e2c429e
Sai Hemanth Gummadapu <hemanth@lidvizion.com>
2026-03-10 16:17:40 -0700

    fix: add font download script for Railway deployment

---
+++ scripts/download-fonts.js
@@ -0,0 +1,35 @@
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
+const fonts = [
+  {
+    url: 'https://github.com/googlefonts/noto-fonts/raw/refs/heads/main/hinted/ttf/NotoSans/NotoSans-Regular.ttf',
+    dest: 'public/fonts/NotoSans-Regular.ttf'
+  },
+  {
+    url: 'https://github.com/googlefonts/noto-fonts/raw/refs/heads/main/hinted/ttf/NotoSansSymbols2/NotoSansSymbols2-Regular.ttf',
+    dest: 'public/fonts/NotoSansSymbols2-Regular.ttf'
+  }
+];
+
+function download(url, dest, redirects = 5) {
+  if (redirects === 0) return console.error('Too many redirects:', url);
+  const file = fs.createWriteStream(dest);
+  https.get(url, res => {
+    if (res.statusCode === 301 || res.statusCode === 302) {
+      file.close();
+      fs.unlinkSync(dest);
+      return download(res.headers.location, dest, redirects - 1);
+    }
+    res.pipe(file);
+  });
+}
+
+fs.mkdirSync('public/fonts', { recursive: true });
+
+fonts.forEach(({ url, dest }) => {
+  if (fs.existsSync(dest)) return;
+  console.log(`Downloading ${dest}...`);
+  download(url, dest);
+});
